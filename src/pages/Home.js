5d7ff1e24dc8bb46f001711c6f5b007251ade25c
oscarl1234 <118151662+oscarl1234@users.noreply.github.com>
2022-11-14 21:03:26 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,24 @@
 
+import { Link } from "react-router-dom";
+import React from "react";
+import { Tag } from 'primereact/tag';
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="title">Top 5 Github Users</h1>
+        <p className="info">Tap the username to see more information</p>
+        {["GrahamCampbell","fabpot","weierophinney","rkh","josh"].map( item =>
+          <Link key={item} className="link" to={`/person/${item}`}>
+            <Tag className="mr-2" severity="info" value={item}></Tag>
+          </Link>  
+        )}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
