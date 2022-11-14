ff968098f72587d7a7dde1a3a9f86eacf52453fb
oscarl1234 <118151662+oscarl1234@users.noreply.github.com>
2022-11-14 21:00:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from "./pages/Home";
+import Person from "./pages/Person";
+
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+function App() {
+  return (
+        <Router>
+            <Route path="/" exact component={Home} />
+            <Route path="/person/:username" exact component={Person} />
+        </Router>
+    );
+}
+
+export default App;
 
